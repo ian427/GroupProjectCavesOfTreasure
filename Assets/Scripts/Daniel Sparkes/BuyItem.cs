b3e5eef404b6c2aa362e678a27fd90f3b6cc2ee7
Feedpompano <bgoodman9899@gmail.com>
2025-04-10 15:38:40 +0100

    Revert "assembly updates"
    This reverts commit 8ef703d83dbb5c7cbfa5263c553b783b2559d3ec.

--- Assets/Scripts/Daniel Sparkes/BuyItem.cs
+++ Assets/Scripts/Daniel Sparkes/BuyItem.cs
@@ -6,37 +6,22 @@ using TMPro;
 
 public class BuyItem : MonoBehaviour
 {
-    ItemManager manager;
-   
     public int buttonIndex;
     public TextMeshProUGUI Label;
     public GameObject Sprite;
     int goldPrice = 0;
     int diamondsPrice = 0;
     int mysticGemsPrice = 0;
-    int itemID;
-    public GameObject CurrentlyDisplayedItem;
+    int[] itemID;
 
     void Start()
     {
-<<<<<<< Updated upstream
         //while (GameObject.Find("Canvas").GetComponent<ItemManager>().itemName[buttonIndex] == "NULL" || GameObject.Find("Canvas").GetComponent<ItemManager>().itemImage[buttonIndex] == "NULL")
         //{
             Label.SetText(GameObject.Find("Canvas").GetComponent<ItemManager>().itemName[buttonIndex]);
             //Sprite = GameObject.Find("Canvas").GetComponent<ItemManager>().itemImage[buttonIndex];
             itemID = GameObject.Find("Canvas").GetComponent<ItemManager>().itemID[buttonIndex];
         //}
-=======
-        manager = GameObject.Find("Canvas").GetComponent<ItemManager>();
-       
-        for (int i = 0; i < manager.TotalItiems; i++)
-        {
-            if (manager.itemList[buttonIndex] == manager.Furniture[i])
-            {
-                itemID = i;
-            }
-        }
->>>>>>> Stashed changes
         if (buttonIndex < 3)
         {
             goldPrice = 200;
@@ -56,13 +41,12 @@ public class BuyItem : MonoBehaviour
 
     public void Buy()
     {
-        if (manager.goldAmount >= goldPrice && manager.diamondsAmount >= diamondsPrice && manager.mysticGemsAmount >= mysticGemsPrice)
+        if (GameObject.Find("Canvas").GetComponent<ItemManager>().goldAmount >= goldPrice && GameObject.Find("Canvas").GetComponent<ItemManager>().diamondsAmount >= diamondsPrice && GameObject.Find("Canvas").GetComponent<ItemManager>().mysticGemsAmount >= mysticGemsPrice)
         {
-            manager.goldAmount -= goldPrice;
-            manager.diamondsAmount -= diamondsPrice;
-            manager.mysticGemsAmount -= mysticGemsPrice;
-            itemID = manager.FindItem(CurrentlyDisplayedItem);
-            manager.AmountOfFurniture[itemID]++;
+            GameObject.Find("Canvas").GetComponent<ItemManager>().goldAmount -= goldPrice;
+            GameObject.Find("Canvas").GetComponent<ItemManager>().diamondsAmount -= diamondsPrice;
+            GameObject.Find("Canvas").GetComponent<ItemManager>().diamondsAmount -= mysticGemsPrice;
+            GameObject.Find("Canvas").GetComponent<ItemManager>().boughtList[itemID[0]][itemID[1]] += 1;
         }
     }
 }
--- Assets/Scripts/Daniel Sparkes/ExitScene.cs
+++ Assets/Scripts/Daniel Sparkes/ExitScene.cs
@@ -5,21 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class ExitScene : MonoBehaviour
 {
-    ItemManager manager;
-    FurnitureData data;
     public string scene;
 
     public void Exit()
     {
-        data = (FurnitureData)Resources.Load("GameData");
-        manager = gameObject.GetComponent<ItemManager>();
-        data.Money[0] = manager.goldAmount;
-        data.Money[1] = manager.diamondsAmount;
-        data.Money[2] = manager.mysticGemsAmount;
-        for (int i = 0; i < data.TotalItiems; i++)
-        {
-            data.AmountOfFurniture[i] += manager.boughtList[i];
-        }
         SceneManager.LoadScene(scene);
     }
 }
