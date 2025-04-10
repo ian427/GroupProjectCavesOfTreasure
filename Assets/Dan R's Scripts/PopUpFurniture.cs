195670e151f483bbd1ae6d02c4b41eaa50e97c18
DanielSparkes <>
2025-04-10 10:30:52 +0100

    Double click function added and tested

---
+++ Assets/Dan R's Scripts/PopUpFurniture.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpFurniture : MonoBehaviour
+{
+    public GameObject popUpMenu;
+
+    //Double clicking variables
+    private float firstClickTime;
+    private float timeInbetweenClicking = 0.5f;
+    private bool isTimeCheckAllowed = true;
+    private int clickNumber = 0;
+
+    //Sets the popUpMenu to be inactive upon the start
+    void Start()
+    {
+        popUpMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            clickNumber += 1;
+        }
+
+        if(clickNumber == 1 && isTimeCheckAllowed)
+        {
+            firstClickTime = Time.time;
+            StartCoroutine(DetectDoubleClick());
+        }
+    }
+
+    private IEnumerator DetectDoubleClick()
+    {
+        isTimeCheckAllowed = false;
+        while(Time.time < firstClickTime + timeInbetweenClicking)
+        {
+            if(clickNumber == 2)
+            {
+                Debug.Log("Double clicked");
+                popUpMenu.SetActive(true); 
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        clickNumber = 0;
+        isTimeCheckAllowed = true;
+    }
+}
