8ef703d83dbb5c7cbfa5263c553b783b2559d3ec
Feedpompano <bgoodman9899@gmail.com>
2025-04-10 15:18:05 +0100

    assembly updates

---
+++ Assets/Nova/Hunt.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Hunt : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Speed at which the object moves
+    public int Scene;
+    private Animator MonsterAnimation;
+    public float moveInput;
+
+    private FurnitureData furnitureData;
+
+    public TMP_Text moneyText;
+    
+
+    
+    public int[] Money;
+    
+
+    void Start()
+    {
+        furnitureData.LoadGameData();
+
+        MonsterAnimation = GetComponent<Animator>();
+        Money= furnitureData.Money ;//SETS furnitur date money as money
+    }
+
+    
+
+    // Update is called once per frame
+    private void Update()
+    {
+        moneyText.text = "Gold " + Money + "";
+
+        // Get horizontal input (left and right arrow keys, A/D, etc.)
+        float moveInput = Input.GetAxis("Horizontal");
+
+        // Move the GameObject left and right
+        transform.Translate(Vector3.right * moveInput * moveSpeed * Time.deltaTime);
+
+        // Flip the GameObject when moving left or right
+        if (moveInput > 0) // Moving right
+        {
+            // Set the localScale's x to positive to face right
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (moveInput < 0) // Moving left
+        {
+            // Set the localScale's x to negative to face left
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        // If the player is moving, play the walking animation
+        if (moveInput != 0)
+        {
+            MonsterAnimation.SetBool("isWalking", true);
+        }
+        else
+        {
+            // If no movement input, stop the walking animation
+            MonsterAnimation.SetBool("isWalking", false);
+        }
+
+        
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the other object has the "Collectible" tag
+        if (other.gameObject.CompareTag("Chest"))
+        {
+            Destroy(other.gameObject);
+            Money[0] = +Random.Range(1, 11);
+            furnitureData.Money = Money;//SETS furnitur date money as money
+            //furnitureData.SavePrefrences();
+
+        }
+
+        if (other.gameObject.CompareTag("Home"))
+        {
+                     
+         
+         SceneManager.LoadScene(Scene);
+
+        }
+    }
+        
+
+}
+
+
--- Assets/Scripts/Daniel Sparkes/ItemManager.cs
+++ Assets/Scripts/Daniel Sparkes/ItemManager.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+<<<<<<< Updated upstream
+=======
+using Unity.VisualScripting;
+using UnityEditor;
+using System.Linq;
+>>>>>>> Stashed changes
 
 public class ItemManager : MonoBehaviour
 {
@@ -11,18 +17,45 @@ public class ItemManager : MonoBehaviour
     public int goldAmount;
     public int diamondsAmount;
     public int mysticGemsAmount;
-    string[] itemNameIndex = { "Tier1Item1", "Tier1Item2", "Tier1Item3", "Tier1Item4", "Tier1Item5", "Tier1Item6", "Tier1Item7", "Tier1Item8", "Tier1Item9", "Tier1Item10", "Tier2Item1", "Tier2Item2", "Tier2Item3", "Tier2Item4", "Tier2Item5", "Tier2Item6", "Tier2Item7", "Tier2Item8", "Tier2Item9", "Tier2Item10", "Tier3Item1", "Tier3Item2", "Tier3Item3", "Tier3Item4", "Tier3Item5", "Tier3Item6", "Tier3Item7" };
-    string[][] itemImageIndex = { new[] { "Tier1Item1.1", "Tier1Item1.2", "Tier1Item1.3", "Tier1Item1.4" }, new[] { "Tier1Item2.1", "Tier1Item2.2", "Tier1Item2.3", "Tier1Item2.4" }, new[] { "Tier1Item3.1", "Tier1Item3.2", "Tier1Item3.3", "Tier1Item3.4" }, new[] { "Tier1Item4.1", "Tier1Item4.2", "Tier1Item4.3", "Tier1Item4.4" }, new[] { "Tier1Item5.1", "Tier1Item5.2", "Tier1Item5.3", "Tier1Item5.4" }, new[] { "Tier1Item6.1", "Tier1Item6.2", "Tier1Item6.3", "Tier1Item6.4" }, new[] { "Tier1Item7.1", "Tier1Item7.2", "Tier1Item7.3", "Tier1Item7.4" }, new[] { "Tier1Item8.1", "Tier1Item8.2", "Tier1Item8.3", "Tier1Item8.4" }, new[] { "Tier1Item9.1", "Tier1Item9.2", "Tier1Item9.3", "Tier1Item9.4" }, new[] { "Tier1Item10.1", "Tier1Item10.2", "Tier1Item10.3", "Tier1Item10.4" }, new[] { "Tier2Item1.1", "Tier2Item1.2" }, new[] { "Tier2Item2.1", "Tier2Item2.2" }, new[] { "Tier2Item3.1", "Tier2Item3.2" }, new[] { "Tier2Item4.1", "Tier2Item4.2" }, new[] { "Tier2Item5.1", "Tier2Item5.2" }, new[] { "Tier2Item6.1", "Tier2Item6.2" }, new[] { "Tier2Item7.1", "Tier2Item7.2" }, new[] { "Tier2Item8.1", "Tier2Item8.2" }, new[] { "Tier2Item9.1", "Tier2Item9.2" }, new[] { "Tier2Item10.1", "Tier2Item10.2" }, new[] { "Tier3Item1" }, new[] { "Tier3Item2" }, new[] { "Tier3Item3" }, new[] { "Tier3Item4" }, new[] { "Tier3Item5" }, new[] { "Tier3Item6" }, new[] { "Tier3Item7" } };
-    public string[] itemName = { "NULL", "NULL", "NULL", "NULL", "NULL", "NULL" };
-    public string[] itemImage = { "NULL", "NULL", "NULL", "NULL", "NULL", "NULL" };
-    public int[][] itemID = { new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 } };
-    public int[][] boughtList = { new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0, 0 }, new[] { 0 }, new[] { 0 }, new[] { 0 }, new[] { 0 }, new[] { 0 }, new[] { 0 }, new[] { 0 } };
+    public GameObject[] itemList = new GameObject[10];
+    public int[] boughtList;
     float timer = 0;
     int days = 6;
-    int[] random = { 0, 0 };
+    int random;
+    public int TotalItiems;
+    [SerializeField] public List<GameObject> Furniture;//needs to be shop variant
+    [SerializeField] public int[] AmountOfFurniture;
+     public List<GameObject> ButtonsT1;
+     public List<GameObject> ButtonsT2;
+    public GameObject ButtonT3;
+    private FurnitureData data;
+    public void UpdateFurniturData()
+    {
+        data.Furniture = Furniture;
+        data.AmountOfFurniture = AmountOfFurniture;
 
+    }
+    public int FindItem(GameObject Item)
+    {
+        int answer = 0;
+        for (int i = 0; i < Furniture.Count; i++)
+        {
+            if (Furniture[i] == Item)
+            {
+                answer = i;
+
+            }
+        }
+        return answer;
+    }
+    public void RemoveItem(GameObject Item)
+    {
+        int temp = FindItem(Item);
+        AmountOfFurniture[temp]--;
+    }
     void Start()
     {
+<<<<<<< Updated upstream
         while (true)
         {
             /*timer += Time.deltaTime;
@@ -58,6 +91,49 @@ public class ItemManager : MonoBehaviour
                     itemID[5][1] = 0;
                 }
             }*/
+=======
+        data = (FurnitureData)Resources.Load("GameData");
+        goldAmount = data.Money[0];
+        diamondsAmount = data.Money[1];
+        mysticGemsAmount = data.Money[2];
+        for (int i = 0; i < data.Furniture.Count; i++)
+        {
+            itemList.Append(data.Furniture[i]);
+        }
+        Furniture = data.Furniture;
+        AmountOfFurniture = data.AmountOfFurniture;
+        TotalItiems = data.TotalShopItiems;
+    }
+
+    private void Update()
+    {
+        //Function to update items on sale
+        timer += Time.deltaTime;
+        if (timer > 1)
+        {
+            timer = 0;
+            days--;
+            //Tier 1
+            for (int i = 0; i < 3; i++)
+            {
+                //Generate random number
+                random = Random.Range(0, 10);
+                ButtonsT1[i].GetComponent<BuyItem>().CurrentlyDisplayedItem = Furniture[random];
+            }
+            //Tier 2
+            for (int i = 0; i < 2; i++)
+            {
+                random = Random.Range(10, 20);
+                ButtonsT2[i].GetComponent<BuyItem>().CurrentlyDisplayedItem = Furniture[random];
+            }
+            //Tier 3
+            if (days < 0)
+            {
+                days = 6;
+                random = Random.Range(20, 27);
+                ButtonT3.GetComponent<BuyItem>().CurrentlyDisplayedItem = Furniture[random];
+            }
+>>>>>>> Stashed changes
         }
     }
 }
