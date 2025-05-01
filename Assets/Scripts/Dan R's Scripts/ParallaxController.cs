794065edf61be0a244eaaa8292e5fd8427f9e0cd
DanielSparkes <>
2025-05-01 09:24:50 +0100

    Adjustments made to background moving

--- Assets/Scripts/Dan R's Scripts/ParallaxController.cs
+++ Assets/Scripts/Dan R's Scripts/ParallaxController.cs
@@ -13,7 +13,7 @@ public class ParallaxController : MonoBehaviour
     float[] backgroundSpeeds;
     float furthestFrame;
 
-    [Range(0.01f, 0.3f)]
+    [Range(0.01f, 1.5f)]
     public float parallaxMovementSpeed;
 
     void Start()
