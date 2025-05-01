383df45fa10bd22d108c29863bfe010b99c5c334
DanielSparkes <S264804@uos.ac.uk>
2025-05-01 15:36:09 +0100

    on quit savegame data

--- Assets/Scripts/IANScODE/ONGAMEQUIT.cs
+++ Assets/Scripts/IANScODE/ONGAMEQUIT.cs
@@ -6,7 +6,7 @@ public class ONGAMEQUIT : MonoBehaviour
 {
     FurnitureData data;
     // Start is called before the first frame update
-    void Start()
+    void OnApplicationQuit()
     {
         data = (FurnitureData)Resources.Load("GameData");
         data.SaveGameData();
