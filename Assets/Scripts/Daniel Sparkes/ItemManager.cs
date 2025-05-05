045bfe4a31748b4f1eab13ff48a3bebd5d6015c2
DanRudkin <161759661+IronDan20@users.noreply.github.com>
2025-05-05 17:14:51 +0100

    Slight adjustments made to the hunt scene

--- Assets/Scripts/Daniel Sparkes/ItemManager.cs
+++ Assets/Scripts/Daniel Sparkes/ItemManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Linq;
-using Unity.PlasticSCM.Editor.WebApi;
+//using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
 using System;
 using System.ComponentModel;
