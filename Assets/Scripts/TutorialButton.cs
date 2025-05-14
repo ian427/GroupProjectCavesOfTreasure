e8b6221c76f5e6708cefb9e2711dbce1635f0f56
Linkgiggy2002 <connordennis19@googlemail.com>
2025-05-14 22:19:25 +0100

    Working Tutorial Button

--- Assets/Scripts/TutorialButton.cs
+++ Assets/Scripts/TutorialButton.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine.Serialization;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+
+
 
 public class TutorialButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject popup;
+
+    public void ClosePopup()
     {
-        
+        popup.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
