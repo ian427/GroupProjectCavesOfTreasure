e0884884be2050bd3c65047f1fc1893bd0295cea
Linkgiggy2002 <connordennis19@googlemail.com>
2025-05-14 22:51:22 +0100

    Tutorial Popups with working buttons

--- Assets/Scripts/TutorialButton.cs
+++ Assets/Scripts/TutorialButton.cs
@@ -4,18 +4,11 @@ using System.Runtime.CompilerServices;
 using UnityEngine.Serialization;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-
-
-
 public class TutorialButton : MonoBehaviour
 {
     [SerializeField] GameObject popup;
-
     public void ClosePopup()
     {
         popup.SetActive(false);
     }
-
-
-
 }
