17aae3787d20aa4d43c272b3a3a7ef0b0c96496f
Linkgiggy2002 <connordennis19@googlemail.com>
2025-05-20 22:54:04 +0100

    TutorialDisabler

--- Assets/Scripts/PopupButton.cs
+++ Assets/Scripts/PopupButton.cs
@@ -4,11 +4,49 @@ using System.Runtime.CompilerServices;
 using UnityEngine.Serialization;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEditor.UIElements;
 public class TutorialButton : MonoBehaviour
 {
     [SerializeField] GameObject popupbutton;
+    [SerializeField] private List<GameObject> popupslist;
+    public bool ShowTutorials = true;
+    
+
+    private void Awake()
+    {
+        //popupslist = new List<GameObject>();
+
+    }
+
+    private void Update()
+    {
+
+        if (ShowTutorials == false)
+        {
+            foreach (GameObject popup in popupslist)
+
+            {
+                popup.SetActive(false);
+
+            }
+        }
+        
+    }
+
     public void ClosePopup()
     {
         popupbutton.SetActive(false);
     }
+
+    public void DisableTutorials()
+    {
+        ShowTutorials = false;
+    }
+
+    public void EnableTutorials()
+    {
+        ShowTutorials = true;
+    }
+
+   
 }
