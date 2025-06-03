a30116d748788dc381390e80de15e0c22957a512
DanielSparkes <161759661+IronDan20@users.noreply.github.com>
2025-06-03 09:48:12 +0100

    Revert "Reverts Changes"
    This reverts commit eb8af818fd27cb6521fbd44850ca282ba63e3428.

--- Assets/Scripts/PopupButton.cs
+++ Assets/Scripts/PopupButton.cs
@@ -4,7 +4,11 @@ using System.Runtime.CompilerServices;
 using UnityEngine.Serialization;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-public class TutorialButton2 : MonoBehaviour
+public class TutorialButton : MonoBehaviour
 {
-   
+    [SerializeField] GameObject popupbutton;
+    public void ClosePopup()
+    {
+        popupbutton.SetActive(false);
+    }
 }
